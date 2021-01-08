9c8cdc37c9b2cb3b3447d101faf9e898ae2cd2a0
ridoansaleh <ridoansalehnasution98@gmail.com>
2021-01-09 03:17:38 +0700

    add response's header properties

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ import { DATABASE_URI, PORT } from "./constant.js";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Expose-Headers", "Content-Length");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Accept, Authorization, Content-Type, X-Requested-With, Range"
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  } else {
+    return next();
+  }
+});
+
 app.use(bodyParser.json());
 
 mongoose.connect(DATABASE_URI, {
