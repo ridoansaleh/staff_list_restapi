20885d1dda6de0c045ddbded0f6b2579e46f3d0f
ridoansaleh <ridoansalehnasution98@gmail.com>
2021-01-09 02:11:12 +0700

    get env PORT value

--- constant.js
+++ constant.js
@@ -4,7 +4,6 @@ dotenv.config();
 const { DB_USERNAME, DB_PASSWORD, DB_NAME, JWT_SECRET } = process.env;
 
 const DATABASE_URI = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@cluster0-shard-00-00.nklo7.mongodb.net:27017,cluster0-shard-00-01.nklo7.mongodb.net:27017,cluster0-shard-00-02.nklo7.mongodb.net:27017/${DB_NAME}?ssl=true&replicaSet=atlas-pk7b7t-shard-0&authSource=admin&retryWrites=true&w=majority`;
-
-const PORT = 3600;
+const PORT = process.env.PORT || 3600;
 
 export { DATABASE_URI, PORT, JWT_SECRET };
